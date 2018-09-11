e731492c147cbd9b94cd0bc3c659ca7357976ae3
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-09-11 16:31:30 -0400

    using req.query in api routes for getting book with title or qenre

--- server/api/books.js
+++ server/api/books.js
@@ -1,36 +1,29 @@
 const router = require('express').Router()
-
 const {Book} = require('../db/models')
 
-router.get('/:genre', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const booksByGenre = await Book.findByGenre(req.params.genre)
-    res.json(booksByGenre)
+    if (req.query) {
+      const books = await Book.findAll({
+        where: req.query
+      })
+      res.status(200).send(books)
+    } else {
+      const books = await Book.findAll()
+      res.status(200).send(books)
+    }
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:title', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
-    const booksByTitle = await Book.findAll({
-      where: {
-        title: req.params.title
-      }
-    })
-    res.status(200).send(booksByTitle)
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.get('/:id', async (req,res,next) => {
-  try {
-    const id = req.params.id;
-    const book = await findById(id)
-    res.send(book);
+    const id = req.params.id
+    const book = await Book.findById(id)
+    res.send(book)
   } catch (error) {
-    next(error);
+    next(error)
   }
 })
 
