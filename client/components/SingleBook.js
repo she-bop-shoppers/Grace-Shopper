2e391111569ba3804cfd3cf50f3378b02c3de607
Katharine Francis <katie-francis@hotmail.com>
2018-09-11 18:04:59 -0400

    Updated SingleBook Component

--- client/components/SingleBook.js
+++ client/components/SingleBook.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import {fetchSingleBook} from '../reducers/book'
+import {connect} from 'react-redux'
 
-const SingleBook = props => {
-  const book = props.book
-
-  return (
-    <div>
-      <h1>{book.title}</h1>
-      <img src={book.imageUrl} />
-      <p>{book.description}</p>
-    </div>
-  )
+class SingleBook extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.book.title}</h1>
+        <img src={this.props.book.imageUrl} />
+        <p>{this.props.book.description}</p>
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    book: state.singleBook
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    singleBook: id => dispatch(fetchSingleBook(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleBook)
