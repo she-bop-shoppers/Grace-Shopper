18b375b0c7ebe55ad09526eaf86dbb13efa912cb
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-09-11 15:41:59 -0400

    api/books/title

--- server/api/books.js
+++ server/api/books.js
@@ -1,3 +1,16 @@
 const router = require('express').Router()
+const {Book} = require('../db')
 
+router.get('/title', async (req, res, next) => {
+  try {
+    const booksByTitle = await Book.findAll({
+      where: {
+        title: req.params.title
+      }
+    })
+    res.status(200).send(booksByTitle)
+  } catch (err) {
+    next(err)
+  }
+})
 module.exports = router
