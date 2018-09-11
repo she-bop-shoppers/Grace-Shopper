5238a1013c71a57a7e3c43311ce6b66e87151229
Gayathri Subramanian <sgayathri88@gmail.com>
2018-09-11 14:06:25 -0400

    merged model updates and material UI

--- demo.js
+++
@@ -1,1 +0,0 @@
-console.log('Gayathri has typed something')
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,32 @@
 const User = require('./user')
+const Book = require('./book')
+const Author = require('./author')
+const Review = require('./review')
+const Sale = require('./sale')
+const OrderItem = require('./orderItem')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+//associations
+Sale.belongsTo(User)
+User.hasMany(Sale)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+OrderItem.belongsTo(Sale)
+Sale.hasMany(OrderItem)
+
+OrderItem.belongsTo(Book)
+Book.hasMany(OrderItem)
+
+Review.belongsTo(User)
+User.hasMany(Review)
+
+Book.belongsTo(Author)
+Author.hasMany(Book)
+
+
 module.exports = {
-  User
+  User,
+  Book,
+  Author,
+  Review,
+  Sale,
+  OrderItem
 }
