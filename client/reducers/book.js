77d9285500a557ec4dbd893f33963f770a3a157b
Gayathri Subramanian <sgayathri88@gmail.com>
2018-09-11 17:42:19 -0400

    reducer logic updated for querying books

--- client/reducers/book.js
+++ client/reducers/book.js
@@ -5,6 +5,7 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_BOOK = 'GET_BOOK'
+const GET_BOOKS_BY_QUERY = 'GET_BOOKS_BY_QUERY'
 const REMOVE_BOOK = 'REMOVE_BOOK'
 
 /**
@@ -12,29 +13,40 @@ const REMOVE_BOOK = 'REMOVE_BOOK'
  */
 const initState = {
   allBooks: [],
-  singleBook: {}
+  singleBook: {},
+  booksMatchingQuery: [],
 }
 
 /**
  * ACTION CREATORS
  */
-const getBook = user => ({type: GET_BOOK, user})
+const getBook = book => ({type: GET_BOOK, book})
+const getBooksByQuery = books => ({type: GET_BOOKS_BY_QUERY, books})
 const removeBook = () => ({type: REMOVE_BOOK})
 
 /**
  * THUNK CREATORS
  */
-
+ export const getQueriedBooks = queryDetails => async dispatch => {
+   try {
+     const {data} = await axios.get(`/api/books?${queryDetails.queryType}=${queryDetails.queryValue}`)
+     dispatch(getBooksByQuery(data))
+   } catch (err) {
+     console.error(err)
+   }
+ }
 
 /**
  * REDUCER
  */
-export default function(state = initState, action) {
+export default function (state = initState, action) {
   switch (action.type) {
     case GET_BOOK:
       return action.book
     case REMOVE_BOOK:
       return action.book
+    case GET_BOOKS_BY_QUERY:
+      return { ...state, booksMatchingQuery: action.books }
     default:
       return state
   }
