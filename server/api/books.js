bf1e6dfbc31f64edfad11d0182e5360df4fe1df4
Jillian McLaren <jillian.mclaren@gmail.com>
2018-09-11 16:25:08 -0400

    api books working for list of all books

--- server/api/books.js
+++ server/api/books.js
@@ -1,3 +1,8 @@
+const Book = require('../db/models/book')
 const router = require('express').Router()
 
+router.get('/', async (req, res, next) => {
+  const books = await Book.findAll()
+  res.json(books)
+})
 module.exports = router
