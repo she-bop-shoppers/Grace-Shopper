af8dcaf9f547c74829c93c98a4c11735dd9e56fc
Jillian McLaren <jillian.mclaren@gmail.com>
2018-09-11 15:05:05 -0400

    server api book file

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Review = db.define('review', {
-  date:{
-    type:Sequelize.Date,
+  date: {
+    type: Sequelize.DATE
   },
-  text:{
-    type:Sequelize.TEXT,
+  text: {
+    type: Sequelize.TEXT
   }
-});
+})
 module.exports = Review
