91162d669b0b0df04b90a1f3a5777a008152b5c0
Katharine Francis <katie-francis@hotmail.com>
2018-09-11 15:29:23 -0400

    Single book api path

--- server/api/books.js
+++ server/api/books.js
@@ -1,3 +1,13 @@
 const router = require('express').Router()
 
+router.get('/:id', async (req,res,next) => {
+  try {
+    const id = req.params.id;
+    const book = await findById(id)
+    res.send(book);
+  } catch (error) {
+    next(error);
+  }
+})
+
 module.exports = router
