74bd2c0edc68e477f563a557f1f8c20fdd817e4d
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-09-12 15:08:34 -0400

    adding react route for allBook and SingleBook

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, ConnectedAllBooks} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  ConnectedAllBooks,
+  SingleBook,
+  allBooks
+} from './components'
 import {me} from './reducers/user'
 
 /**
@@ -21,6 +28,9 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route exact path="/books" component={allBooks} />
+        <Route path="/books/:bookId" component={SingleBook} />
+
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
