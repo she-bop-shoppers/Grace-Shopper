35c77d5398f3899112eef5b0114a7c852dcb7815
Gayathri Subramanian <sgayathri88@gmail.com>
2018-09-15 14:54:45 -0400

    added OAUTH functionality and updated SingleGenre to eagerload author details

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -31,13 +31,11 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password')
-    },
-    allowNull: false
+    }
   },
   salt: {
     type: Sequelize.STRING,
