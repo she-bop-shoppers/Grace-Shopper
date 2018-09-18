40b4c75db69391ad3f0069187d382480072c312a
Gayathri Subramanian <sgayathri88@gmail.com>
2018-09-18 12:39:08 -0400

    more changes to seed

--- script/seed.js
+++ script/seed.js
@@ -752,34 +752,18 @@ const seed = async () => {
     })
   )
   console.log('seeding success!')
+  await db.close()
 }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
 // The `seed` function is concerned only with modifying the database.
-seed()
-  .catch(err => {
-    console.error('Oh noes! Something went wrong!')
-    console.error(err)
-  })
-  .finally(completed => {
-    console.log('made it to the finally section')
-    return db.close()
-  })
+seed().catch(err => {
+  console.error('Oh noes! Something went wrong!')
+  console.error(err)
+  db.close()
+})
 
-// async function runSeed() {
-//   console.log('seeding...')
-//   try {
-//     await seed()
-//   } catch (err) {
-//     console.error(err)
-//     process.exitCode = 1
-//   } finally {
-//     console.log('closing db connection')
-//     await db.close()
-//     console.log('db connection closed')
-//   }
-// }
 //
 // // Execute the `seed` function, IF we ran this module directly (`node seed`).
 // // `Async` functions always return a promise, so we can use `catch` to handle
